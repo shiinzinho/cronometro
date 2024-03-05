b807c0d54cc25b889c9d7b68ba9dc00ae7d02bef
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-03-05 15:22:50 -0300

    UPDATE

--- App.tsx
+++ App.tsx
@@ -2,19 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 function App(): React.JSX.Element {
+
+  let ss: number = 0;
+  let mm: number = 0;
+  let hh: number = 0;
+
+  const [botao, setBotao] = useState('Iniciar');
+  const [ultimo, seUultimo] = useState('00:00:00');
+  const [formattedTime, setFormattedTime] = useState('00:00:00');
+  const [interbalId, setInterbalId] = useState<NodeJS.Timeout | null>(null);
+
   return (
     <View style={styles.container}>
       <Image
         source={require('./src/assets/images/crono.png')}
       />
       <Text style={styles.timer}>00:00:00</Text>
-      <View style={styles.btnArea} />
+      <View style={styles.btnArea}>
       <TouchableOpacity style={styles.btn}>
         <Text style={styles.btnTexto}>Iniciar</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.btn}>
         <Text style={styles.btnTexto}>Limpar</Text>
       </TouchableOpacity>
+      </View>
       <View style={styles.areaTempo}>
         <Text style={styles.textCorrida}>
           Ultimo Tempo: 00:00:00
@@ -24,27 +35,46 @@ function App(): React.JSX.Element {
   );
 }
 const styles = StyleSheet.create({
-  container: {
-
-  },
-  areaTempo: {
-
-  },
-  textCorrida: {
-
-  },
-  timer: {
-
-  },
-  btn: {
-
-  },
-  btnTexto: {
-
-  },
-  btnArea: {
-
-  }
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#1e1e1e',
+    },
+    areaTempo: {
+      marginTop: 30,
+    },
+    textCorrida: {
+      fontSize: 20,
+      color: '#ffffff',
+      fontStyle: 'italic'
+    },
+    timer: {
+      marginTop: -160,
+      fontSize: 45,
+      fontWeight: 'bold',
+      color: '#ffffff',
+      marginBottom: 20,
+    },
+    btn: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFF',
+      height: 40,
+      width: 17,
+      borderRadius: 20,
+    },
+    btnTexto: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      color: '#1e1e1e',
+    },
+    btnArea: {
+      flexDirection: 'row',
+      marginTop: 130,
+      height: 40,
+    },
+  });
 
 export default App;
